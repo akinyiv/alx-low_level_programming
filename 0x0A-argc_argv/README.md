a3c75e29d528d4c0dbc90a4dc686b032c6639cfa
akinyiv <akinyivee10@gmail.com>
2022-04-01 19:02:06 +0300

    name

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+0- Program that prints its name, followed by a new line.
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int main(int argc __attribute__((unused)), char *argv[]);
+
+#endif /* MAIN_H */
