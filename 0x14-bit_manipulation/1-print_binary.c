98284f037d1ae8cfe045df4d14af59beabe9ca53
akinyiv <akinyivee10@gmail.com>
2022-04-28 17:08:36 +0300

    bit maipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,24 @@
 #include "main.h"
-
 /**
- * print_binary - prints the binary representation
- * of a number.
- * @n: unsigned long int.
- *
- * Return: no return.
- */
+  *print_binary - prints the binary representation of a number.
+  *@n: unsigned long int to be converted.
+  *
+  *Return: void.
+  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
+	unsigned long int mask;
+
+	if (n == 0)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		_putchar('0');
 	}
 	else
 	{
-		_putchar('0');
+		mask = n & 1;
+		n = n >> 1;
+		if (!(n == 0))
+			print_binary(n);
+		_putchar(mask + '0');
 	}
 }
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,2 +1,3 @@
 0- Function that converts a binary number to an unsigned int.
 1- Function that prints the binary representation of a number.
+2- Function that returns the value of a bit at a given index.
