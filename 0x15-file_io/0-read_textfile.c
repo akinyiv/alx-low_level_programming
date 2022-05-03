0b5f9bba0cbd9c11d1ce11a834b892ad26ab59c1
akinyiv <akinyivee10@gmail.com>
2022-05-03 15:50:04 +0300

    file io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,36 @@
-
-  
-#include "main.h"
+#include "holberton.h"
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- *
- * Return: the number of letters printed, or 0 if it failed
- */
+  * read_textfile - reads a text file and prints to the POSIX stdout.
+  * @filename: pointer to name of file.
+  * @letters:  number of letters it should read and print.
+  *
+  * Return: number of letters it could print or write or 0.
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-int s, t;
-char *buf;
+	int file, rd, wr;
+	char *buf;
 
-if (!filename)
-return (0);
+	if (filename == NULL)
+		return (0);
 
-fd = open(filename, O_RDONLY);
-if (fd < 0)
-return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+		return (0);
+	rd = read(file, buf, letters);
+	if (rd == -1)
+		return (0);
 
-buf = malloc(sizeof(char) * letters);
-if (!buf)
-return (0);
+	buf[letters] = '\0';
 
-s = read(fd, buf, letters);
-if (s < 0)
-{
-free(buf);
-return (0);
-}
-buf[s] = '\0';
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+		return (0);
 
-close(fd);
-
-t = write(STDOUT_FILENO, buf, s);
-if (t < 0)
-{
-free(buf);
-return (0);
-}
-
-free(buf);
-return (t);
+	close(file);
+	free(buf);
+	return (wr);
 }
